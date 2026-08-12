f59bc7a6a4d2cc38c33fb3cbd8cb356444ef6f5a
sham_bhujbal <sbhujbal03@gmail.com>
2026-08-12 22:08:46 +0530

    Initial commit

---
+++ src/main/java/com/company/store/AppCustomerWebPortalApplication.java
@@ -0,0 +1,11 @@
+package com.company.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCustomerWebPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppCustomerWebPortalApplication.class, args);
+    }
+}
